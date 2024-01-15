24cc87c88f1b8d7c064a490bf16d039014dce760
Pratik Devnal <79634014+pratikdevnal@users.noreply.github.com>
2024-01-16 00:54:04 +0530

    added Random component

--- src/components/Random.js
+++ src/components/Random.js
@@ -4,18 +4,22 @@ const API_KEY = process.env.REACT_APP_GIPHY_APP_KEY;
 export default function Random() {
   async function fetchData() {
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const output = await axios.get(url);
-    console.log(output);
+    const { data } = await axios.get(url);
+    const imageSourse = data.data.images.downsized_large.url;
+    console.log(imageSourse);
+    setGif(imageSourse);
   }
   useEffect(() => {
     fetchData();
   }, []);
   const [gif, setGif] = useState("");
-  let clickHandler = () => {};
+  let clickHandler = () => {
+    fetchData();
+  };
   return (
-    <div className="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <div className="w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 my-[15px] py-10">
       <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
-      <img src={gif} alt="" srcset="" width="450" />
+      <img src={gif} alt="" srcset="" />
       <button
         className="bg-white w-10/12 opacity-70 border border-black text-lg py-2 rounded-lg"
         onClick={clickHandler}
